6944b946e9eaffe76662b1fd0c628afea06c269b
Ashyzan <info@studiocreativo69.it>
2024-03-14 12:17:18 +0100

    ADD: parseFLoat kmElement and ageElement

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
 console.log('funziona') ;
 
 // recuperare i km dall’input dei km
-
 const kmElement = document.getElementById ('Kilometri'); // object | null
-console.dir(parseInt(kmElement));
+console.dir(kmElement);
+
+// trasformare kmElement in numero
+const km = parseFloat(kmElement.value);
+
 
 // recuperare l’eta dall’input dell’età
-
 const etaElement = document.getElementById ('age');
 console.dir(etaElement);
 
-// Collegare il bottone invio
+// trasformare etaElement in numero
+const eta = parseFloat(etaElement.value);
 
+
+// Collegare il bottone invio
 const buttonElement = document.getElementById('submit'); //
 console.dir(buttonElement);
 
+// attivare ascolto sul submit del form
 
-// trasformare i dati in numeri
-
-// const etaElementNumber = parseInt(kmElement);
-// console.log(etaElementNumber)
-// const kmElementNumber = parseInt(kmElement);
-
-// calcolare il prezzo
-
-// let prezzobase = 0.21* kmElementNumber
-// console.log(prezzobase)
-
-// if (etaElementNumber > 18 && ) {
-
-// }
-
-// else () {
-
-// }
-
-// stampare il prezzo nella pagina html in un semplice div
-
-buttonElement.addEventListener('click', function () {
+buttonElement.addEventListener('submit', function (event) {
     console.log('click sul button')
 
 })
+
+// calcolare il prezzo base
+let prezzobase = 0.21 * kmElement
+console.log(prezzobase)
+
